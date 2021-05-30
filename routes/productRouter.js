76a7f3ef5fcb9cae4dd0af666cd1e31cfb1ea80b
GrietM <marcelagriet@gmail.com>
2021-05-30 03:41:46 -0300

    Queda funcionando el tema de los permisos en index app.all. Modifico las rutas que requieren permisos a '/admin' y excluyo al login. Ya lo implementé tambien en el Frontend para que no se romparn los pedidos de axios

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -8,11 +8,11 @@ const routes = (Product) => {
   const productRouter = express.Router()
   const controller = productsController(Product)
 
-  productRouter.route('/products')
+  productRouter.route('/admin/products')
     .get (validator.query(productsValidation.productsValidationQuery),controller.getProducts)    
     .post (validator.body(productsValidation.productsValidationBody),controller.postProducts) 
 
-  productRouter.route('/products/:productId')
+  productRouter.route('/admin/products/:productId')
   .get(validator.params(productsValidation.productsValidationParams),controller.getProductById)
   .put(validator.params(productsValidation.productsValidationParams),validator.body(productsValidation.productsValidationPut),controller.putProductById)
   .delete(validator.params(productsValidation.productsValidationParams), controller.deleteProductById)
